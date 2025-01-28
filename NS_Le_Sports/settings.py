3939989b7f527966df888d1b90e355e0dbc5266d
ANKUSH SUNIL <137211060+ankuxsshh@users.noreply.github.com>
2025-01-28 12:25:09 +0530

    Update settings.py

--- NS_Le_Sports/settings.py
+++ NS_Le_Sports/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-2yb#*8(3(ane@ba(&ydq3r^-+o8l3wk!#7j6b3m@a=u!em1!$9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
